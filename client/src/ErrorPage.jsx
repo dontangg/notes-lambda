e5b1a8bb2f0a4a9d834cee29af3300870320772e
Don Wilson <rdwilson@ancestry.com>
2024-09-26 14:43:23 -0600

    Add a slightly nicer error page

--- client/src/ErrorPage.jsx
+++ client/src/ErrorPage.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
 	const error = useRouteError();
 	console.error(error);
 
+	const is404 = error.status === 404;
+
 	return (
-		<div id="error-page">
-			<h1>Oops!</h1>
-			<p>Sorry, an unexpected error has occurred.</p>
-			<p>
-				<i>{error.statusText || error.message}</i>
-			</p>
-		</div>
+		<>
+			<nav className="navbar navbar-expand-md bg-body-tertiary">
+				<div className="container">
+					<Link className="navbar-brand" to="/">Notes</Link>
+				</div>
+			</nav>
+			<div className="bg-body-secondary vh-100 d-flex align-items-center ">
+				<div className="container text-center">
+					<h1>Oops!</h1>
+					{is404
+						? (<p>The page you are looking for does not exist. Try going <Link to="/">home</Link>.</p>)
+						: (<p>Something bad happened. Maybe try <a href="javascript:window.location.reload()">refreshing the page</a>?</p>)
+					}
+					
+				</div>
+			</div>
+		</>
 	);
-}
+};
--- client/src/features/signIn/SignInPage.jsx
+++ client/src/features/signIn/SignInPage.jsx
@@ -31,51 +31,49 @@ export default function SignInPage() {
 	const isPasswordValid = !didSubmit || password;
 
 	return (
-		<div className="signInPage d-flex align-items-center vh-100 bg-body-secondary">
-			<div className="vw-100">
-				<form className="container bg-body border rounded border-secondary-subtle p-4" novalidate>
-					<h1>
-						<svg width="40" height="45" viewBox="0 0 240 240" xmlns="http://www.w3.org/2000/svg">
-							<path id="Path" fill="#000000" fill-rule="evenodd" stroke="none" d="M 85 50 L 212 18 L 212 165 C 212 165 208.298538 194 178 194 C 153 194 143.065262 176.044418 143 163 C 142.918457 146.700684 159.817871 132.783203 177 133 C 191.514847 133.183136 200 141 200 141 L 200 63 L 97 88 L 97 198 C 97 198 89.87793 221.59082 63 222 C 36.12207 222.40918 28.060547 202.171875 28 192 C 27.939453 181.828125 39.128101 162.322052 63 162 C 79.577148 161.776367 85 170 85 170 L 85 50 Z" />
-						</svg>
-						<span className="ps-2">Sign in</span>
-					</h1>
-					<div class="input-group mb-3 has-validation">
-						<div className={'form-floating' + (!isEmailValid ? ' is-invalid' : '')}>
-							<input type="email"
-								className={'form-control' + (!isEmailValid ? ' is-invalid' : '')}
-								id="emailInput"
-								placeholder="name@example.com"
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-								required />
-							<label for="emailInput">Email address</label>
-						</div>
-						<div class="invalid-feedback">
-							Please enter your email address.
-						</div>
+		<div className="signInPage d-flex align-items-center vh-100 vw-100 bg-body-secondary">
+			<form className="container bg-body border rounded border-secondary-subtle p-4" novalidate>
+				<h1>
+					<svg width="40" height="45" viewBox="0 0 240 240" xmlns="http://www.w3.org/2000/svg">
+						<path id="Path" fill="#000000" fill-rule="evenodd" stroke="none" d="M 85 50 L 212 18 L 212 165 C 212 165 208.298538 194 178 194 C 153 194 143.065262 176.044418 143 163 C 142.918457 146.700684 159.817871 132.783203 177 133 C 191.514847 133.183136 200 141 200 141 L 200 63 L 97 88 L 97 198 C 97 198 89.87793 221.59082 63 222 C 36.12207 222.40918 28.060547 202.171875 28 192 C 27.939453 181.828125 39.128101 162.322052 63 162 C 79.577148 161.776367 85 170 85 170 L 85 50 Z" />
+					</svg>
+					<span className="ps-2">Sign in</span>
+				</h1>
+				<div class="input-group mb-3 has-validation">
+					<div className={'form-floating' + (!isEmailValid ? ' is-invalid' : '')}>
+						<input type="email"
+							className={'form-control' + (!isEmailValid ? ' is-invalid' : '')}
+							id="emailInput"
+							placeholder="name@example.com"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required />
+						<label for="emailInput">Email address</label>
 					</div>
-					<div class="input-group mb-3 has-validation">
-						<div className={'form-floating' + (!isPasswordValid ? ' is-invalid' : '')}>
-							<input type="password"
-								className={'form-control' + (!isPasswordValid ? ' is-invalid' : '')}
-								id="passwordInput"
-								placeholder="Password"
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-								required />
-							<label for="passwordInput">Password</label>
-						</div>
-						<div class="invalid-feedback">
-							Please enter your password.
-						</div>
+					<div class="invalid-feedback">
+						Please enter your email address.
 					</div>
-					{signInState.error && (<div class="alert alert-danger" role="alert">{signInState.error}</div>)}
-					<button type="submit" class="btn btn-primary" disabled={!email || !password} onClick={onSignInClick}>
-						Sign in {signInState.signInStatus === FetchStatus.pending ? (<Spinner />) : null}
-					</button>
-				</form>
-			</div>
+				</div>
+				<div class="input-group mb-3 has-validation">
+					<div className={'form-floating' + (!isPasswordValid ? ' is-invalid' : '')}>
+						<input type="password"
+							className={'form-control' + (!isPasswordValid ? ' is-invalid' : '')}
+							id="passwordInput"
+							placeholder="Password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required />
+						<label for="passwordInput">Password</label>
+					</div>
+					<div class="invalid-feedback">
+						Please enter your password.
+					</div>
+				</div>
+				{signInState.error && (<div class="alert alert-danger" role="alert">{signInState.error}</div>)}
+				<button type="submit" class="btn btn-primary" disabled={!email || !password} onClick={onSignInClick}>
+					Sign in {signInState.signInStatus === FetchStatus.pending ? (<Spinner />) : null}
+				</button>
+			</form>
 		</div>
 	);
 }
