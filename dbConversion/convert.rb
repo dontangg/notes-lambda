d008aef7ebf0ef3afa6b546cada29814b8a53c7d
Don Wilson <rdwilson@ancestry.com>
2024-09-28 01:10:00 -0600

    Keep database conversion files around for posterity

---
+++ dbConversion/convert.rb
@@ -0,0 +1,60 @@
+def convert(id)
+    comp = Competition.find(id)
+    if comp.submitting?
+        phase = "submitting"
+    elsif comp.guessing?
+        phase = "guessing"
+    else
+        phase = "closed"
+    end
+    songs = comp.songs.map {|song|
+        <<TEXT.chomp
+        {
+            "artist": "#{song.artist}",
+            "title": "#{song.name}",
+            "filename": "#{song.file_name}",
+            "extension": "#{song.extension}",
+            "userId": #{song.user.id}
+        }
+TEXT
+    }.join(",\n")
+    attempts = comp.attempts.order(:created_at).map {|attempt|
+        guesses = attempt.guesses.map {|guess|
+            <<TEXT.chomp
+                {
+                    "songFilename": "#{guess.song.file_name}",
+                    "guessedUserId": #{guess.user.id}
+                }
+TEXT
+        }.join(",\n")
+        <<TEXT.chomp
+        {
+            "createdAt": "#{attempt.created_at.as_json}",
+            "correctCount": "#{attempt.correct_count}",
+            "userId": "#{attempt.user_id}",
+            "guesses": [
+#{guesses}
+            ]
+        }
+TEXT
+    }.join(",\n")
+    json = <<TEXT
+{
+    "pk": "Competition",
+    "sk": "#{comp.name}",
+    "isActive": #{comp.active? ? "true" : "false"},
+    "phase": "#{phase}",
+    "songs": [
+#{songs}
+    ],
+    "attempts": [
+#{attempts}
+    ]
+}
+TEXT
+    puts json.squish
+    nil
+end
+
+# Make sure that there are no extra "\n" in there
+# FYI, competitions are about 24kb this way
